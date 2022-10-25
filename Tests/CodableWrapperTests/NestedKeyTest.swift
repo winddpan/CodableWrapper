c3ab4f68918b85fa8b52e7e3840e1d22a636d0aa
PAN <panxiaoping@yonrun.com>
2022-10-25 15:56:23 +0800

    add noNested test case

--- Tests/CodableWrapperTests/NestedKeyTest.swift
+++ Tests/CodableWrapperTests/NestedKeyTest.swift
@@ -12,6 +12,7 @@ class NestedKeyTest: XCTestCase {
     let JSON = """
     {
         "title": "The Big Bang Theory",
+        "actor.actor_name": "just a test",
         "actor": {
             "actor_name": "Sheldon Cooper",
             "iq": 140
@@ -30,6 +31,9 @@ class NestedKeyTest: XCTestCase {
             @Codec("actor.actor_name")
             var actorName2: String? = nil
 
+            @Codec("actor.actor_name", noNested: true)
+            var noNestedActorName: String? = nil
+
             @Codec("actor.iq")
             var iq: Int = 0
         }
@@ -37,6 +41,7 @@ class NestedKeyTest: XCTestCase {
         let model = try JSONDecoder().decode(Episode.self, from: JSON.data(using: .utf8)!)
 
         XCTAssertEqual(model.show, "The Big Bang Theory")
+        XCTAssertEqual(model.noNestedActorName, "just a test")
         XCTAssertEqual(model.actorName1, "Sheldon Cooper")
         XCTAssertEqual(model.actorName2, "Sheldon Cooper")
         XCTAssertEqual(model.iq, 140)
@@ -46,6 +51,7 @@ class NestedKeyTest: XCTestCase {
         let actor = (jsonObject["actor"] as? [String: Any])
 
         XCTAssertEqual(jsonObject["title"] as? String, "The Big Bang Theory")
+        XCTAssertEqual(jsonObject["actor.actor_name"] as? String, "just a test")
         XCTAssertEqual(actor?["actorName"] as? String, "Sheldon Cooper")
         XCTAssertEqual(actor?["actor_name"] as? String, "Sheldon Cooper")
         XCTAssertEqual(actor?["iq"] as? Int, 140)
