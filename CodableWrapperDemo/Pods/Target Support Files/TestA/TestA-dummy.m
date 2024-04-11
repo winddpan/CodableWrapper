48561031dc046e70c4a25e3daa61e5f84a1af332
陈钰 <chenyu04@kanzhun.com>
2024-04-11 20:50:33 +0800

    修改路径

---
+++ CodableWrapperDemo/Pods/Target Support Files/TestA/TestA-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_TestA : NSObject
+@end
+@implementation PodsDummy_TestA
+@end
---
+++ CodableWrapperDemo/TestA/TestA/TestA.h
@@ -0,0 +1,12 @@
+//
+//  POEBasic.h
+//  Pods
+//
+//  Created by 易雄 on 2024/2/7.
+//
+
+#ifndef TestA_h
+#define TestA_h
+
+
+#endif /* POEBasic_h */
